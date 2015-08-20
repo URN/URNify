7511a0362ebae49db5438038b5e9e4b3f64442f6
James Turner <me@jamesturner.im>
2015-08-20 23:19:15 +0100

    Add basic api functionality and add the /api/schedule/week endpoint

--- urnify.php
+++ urnify.php
@@ -63,6 +63,8 @@ class URNify {
         // http://justintadlock.com/archives/2011/10/20/custom-user-taxonomies-in-wordpress#comment-568682
         // https://gist.github.com/codekipple/2420042
         add_filter('parent_file', array($this, 'fix_taxonomy_menu_jumping'));
+
+        $this->init_api();
     }
 
     public function init_shows() {
@@ -554,6 +556,112 @@ class URNify {
         }
      
         return $parent_file;
+    }
+
+    function init_api() {
+        
+        add_action( 'init', function () {
+            add_rewrite_rule('api/schedule/week', 'index.php?api=schedule_week', 'top' );
+            add_rewrite_rule('api/schedule/day/(monday|tuesday|wednesday|thursday|friday)', 'index.php?api=schedule_$matches[1]', 'top');
+        });
+
+        add_filter('query_vars', function ($query_vars) {
+            $query_vars[] = 'api';
+            return $query_vars;
+        });
+
+        add_filter('template_include', function ($template) {
+            global $wp_query;
+
+            // You could normally swap out the template WP wants to use here, but we'll just die
+            if ( isset( $wp_query->query_vars['api'] ) && !is_page() && !is_single() ) {
+                $wp_query->is_404 = false;
+                $wp_query->is_archive = true;
+                $wp_query->is_category = true;
+
+                $all_options = wp_load_alloptions();
+                $show_options = array();
+                foreach ($all_options as $key => $value) {
+                    if (preg_match('/^show_\d*_custom_option_\w*$/', $key)) {
+                        $show_options[$key] = $value;
+                    }
+                }
+
+                $shows = get_terms('shows', array('hide_empty' => 0));
+                $schedule = array();
+                $schedule['monday'] = array();
+                $schedule['tuesday'] = array();
+                $schedule['wednesday'] = array();
+                $schedule['thursday'] = array();
+                $schedule['friday'] = array();
+                $schedule['saturday'] = array();
+                $schedule['sunday'] = array();
+
+                foreach ($shows as $key => $show) {
+                    $options = array();
+                    foreach ($show_options as $key => $option) {
+                        if (preg_match('/^show_' . $show->term_id . '_custom_option_\w*$/', $key)) {
+                            if ($option === 'false') {
+                                $option = false;
+                            }
+                            if ($option === 'true') {
+                                $option = true;
+                            }
+                            $options[str_replace('show_' . $show->term_id . '_custom_option_', '', $key)] = $option;
+                        }
+                    }
+
+                    if ($options['ended'] || $options['hidden']) {
+                        continue;
+                    }
+
+                    $options['slot'] = unserialize($options['slot']);
+
+                    $show_info = array();
+                    $show_info['name'] = $show->name;
+                    $show_info['slug'] = $show->slug;
+                    $show_info['description'] = $show->description;
+
+                    foreach ($options['slot'] as $key => $slot) {
+                        $day = $slot['day'];
+                        $from = $slot['from'];
+                        $to = $slot['to'];
+
+                        $show_info['from'] = $from;
+                        $show_info['to'] = $to;
+
+                        $show_info['live'] = live($day, $from, $to) ? true : false;
+                        $schedule[strtolower($day)][] = $show_info;
+                    }
+                }
+
+                header('Content-Type: application/json');
+                die(json_encode($schedule));
+            } else {
+                return $template;
+            }
+        });
+
+        function live($day, $from, $to) {
+            $current_day = date('l');
+
+            if ($day !== $current_day) {
+                return false;
+            }
+
+            $from = strtotime($from);
+            $to = strtotime($to);
+            $current = strtotime('now');
+
+            // Stop it thinking 'to' is at the start of the day if it's 'before' the start time
+            if ($from > $to) {
+                $to = strtotime('+1 day', $to);
+            }
+
+            if ($current < $to && $current > $from) {
+                return true;
+            }
+        }
     }
 }
 
