3c01e9da36bf63e6da2ab692f7e0f6b41a9a2176
James Turner <me@jamesturner.im>
2016-02-17 01:12:44 +0000

    Add support for passing new song data to api

--- CurrentSongEndpoint.php
+++ CurrentSongEndpoint.php
@@ -1,6 +1,17 @@
 <?php
 
 class CurrentSongEndpoint extends Endpoint {
+    private function checkMultipleIssetPost($parameters) {
+        $valid = true;
+        foreach ($parameters as $parameter) {
+            if (!isset($_POST[$parameter])) {
+                $valid = false;
+            }
+        }
+
+        return $valid;
+    }
+
     public function get_output($api_name_info) {
         $updateKey = 'MwaOOmqItO2rTNSH0^SypV%C3$5*J#FV!cfSSGLY2K@%Ube9%b#OIRy';
         $method = $_SERVER['REQUEST_METHOD'];
@@ -19,16 +30,29 @@ class CurrentSongEndpoint extends Endpoint {
         if ($method === 'POST') {
             $postedKey = urldecode($_POST['key']);
 
-            if (isset($_POST['key']) && isset($_POST['update']) && $postedKey === $updateKey) {
-                $song = json_decode(file_get_contents('http://www.urn1350.net/current-song.json'));
-                update_option('current_song_title', stripslashes($song->title));
-                update_option('current_song_artist', stripslashes($song->artist));
-                update_option('current_song_image_url', stripslashes($song->image));
-                update_option('current_song_large_image_url', stripslashes($song->image_large));
-                update_option('current_song_start_time', stripslashes($song->start_time));
-                update_option('current_song_duration', stripslashes($song->length));
+            if (isset($_POST['key']) && $postedKey === $updateKey) {
+                if (isset($_POST['update'])) {
+                    $song = json_decode(file_get_contents('http://www.urn1350.net/current-song.json'));
+                    update_option('current_song_title', stripslashes($song->title));
+                    update_option('current_song_artist', stripslashes($song->artist));
+                    update_option('current_song_image_url', stripslashes($song->image));
+                    update_option('current_song_large_image_url', stripslashes($song->image_large));
+                    update_option('current_song_start_time', stripslashes($song->start_time));
+                    update_option('current_song_duration', stripslashes($song->length));
 
-                return array("status" => "success", "message" => "Current song successfully updated");
+                    return array("status" => "success", "message" => "Current song successfully updated");
+                }
+
+                if (checkMultipleIssetPost(array('title', 'artist', 'start_time', 'length', 'image', 'image_large'))) {
+                    update_option('current_song_title', stripslashes($_POST['title']));
+                    update_option('current_song_artist', stripslashes($_POST['artist']));
+                    update_option('current_song_image_url', stripslashes($_POST['image']));
+                    update_option('current_song_large_image_url', stripslashes($_POST['image_large']));
+                    update_option('current_song_start_time', stripslashes($_POST['start_time']));
+                    update_option('current_song_duration', stripslashes($_POST['length']));
+
+                    return array("status" => "success", "message" => "Current song successfully updated");
+                }
             }
             else {
                 return array("status" => "error", "message" => "Invalid update key");
