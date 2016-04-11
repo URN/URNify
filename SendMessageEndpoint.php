e6d1f6052073e8d0894b4ad23c49c2e4dfc114cd
James Turner <me@jamesturner.im>
2016-04-11 19:34:25 +0100

    Send message of type 'android' when android user agent is detected

--- SendMessageEndpoint.php
+++ SendMessageEndpoint.php
@@ -4,7 +4,14 @@ class SendMessageEndpoint extends Endpoint {
     public function get_output($api_name_info) {
         if (isset($_POST['message'])) {
             $sender_ip = $_SERVER['REMOTE_ADDR'];
-            $url = 'http://int.urn1350.net:8080/web/submit_message.php?type=web&message=' . urlencode($_POST['message']);
+
+            $type = 'web';
+
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'URN Android') !== false) {
+                $type = 'android';
+            }
+
+            $url = 'http://int.urn1350.net:8080/web/submit_message.php?type=' . $type . '&message=' . urlencode($_POST['message']);
             $url .= '&sender=' . urlencode($sender_ip);
             $data = file_get_contents($url);
 
